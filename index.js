d3e160c9ffbbfd453930653515839e09fff1f764
AlexRios <riosalexandra91@gmail.com>
2022-10-22 08:22:44 -0500

    responsive health bar

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
     draw() {
         c.fillStyle = 'this.color'
@@ -170,7 +171,8 @@ function animate() {
         player.isAttacking
     ) {
         player.isAttacking = false
-        console.log('go')
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + "%"
     }
 
     if(
@@ -181,7 +183,8 @@ function animate() {
         enemy.isAttacking
     ) {
         enemy.isAttacking = false
-        console.log('enemy attack successful')
+        player.health -= 20
+        document.querySelector('#enemyHealth').style.width = player.health + "%"
     }
 }
 
